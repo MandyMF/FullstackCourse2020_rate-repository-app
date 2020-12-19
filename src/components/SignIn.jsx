5fe1af0159a6785d6f4b829932330720c1c03b41
MandyMF <mandy@fonoma.com>
2020-12-19 04:49:08 -0500

    Exercise 10.8

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,56 @@
 import React from 'react';
-
+import { Alert, View, TouchableWithoutFeedback, StyleSheet } from 'react-native';
+import { Formik } from 'formik';
+import FormikTextInput from './FormikTextInput'
 import Text from './Text';
+import theme from '../theme';
+
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const styles = StyleSheet.create({
+  button: {
+    color: theme.colors.tabLabel,
+    backgroundColor: theme.colors.primary,
+    padding: 10,
+    borderRadius: 5,
+    textAlign: 'center',
+  },
+  container: {
+    padding: 10,
+    backgroundColor: theme.colors.listItemBackgroundColor,
+  },
+  textInput: {
+    color: theme.colors.textSecondary,
+    borderColor: theme.colors.mainBackgroundColor,
+    borderRadius: 5,
+    borderWidth: 1,
+    padding: 10,
+    marginBottom: 10
+  }
+
+});
 
 const SignIn = () => {
-  return <Text>The sign in view</Text>;
+  const onSubmit = (values) => {
+    console.log(values);
+  };
+
+  return (
+    <Formik initialValues={initialValues} onSubmit={onSubmit}>
+      {({ handleSubmit }) =>
+        <View style={styles.container}>
+          <FormikTextInput style={styles.textInput} name='username' placeholder='Username' />
+          <FormikTextInput style={styles.textInput} secureTextEntry name='password' placeholder='Password' />
+          <TouchableWithoutFeedback onPress={handleSubmit}>
+            <Text style={styles.button} fontWeight='bold' fontSize='textSecondary'>Sign in</Text>
+          </TouchableWithoutFeedback>
+        </View>
+      }
+    </Formik>
+  );
 };
 
 export default SignIn;
